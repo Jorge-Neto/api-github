7e49ab0aaf9eb445ca296641f7c2897183a9027c
Jorge Neto <53627696+Jorge-Neto@users.noreply.github.com>
2022-02-09 19:11:23 -0300

    Construção da página inicial

--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { MdSearch } from 'react-icons/md';
 
@@ -6,13 +6,19 @@ import { Container, Logo, Title, Form, Input, Button } from './styles';
 import githubLogo from '../../assests/images/github-logo.svg';
 
 function MainPage() {
+  const [login, setLogin] = useState('');
+
   return (
     <Container>
       <Logo src={githubLogo} alt="Logo API Github" />
       <Title>API GitHub</Title>
       <Form>
-        <Input placeholder="User" />
-        <Button>
+        <Input
+          placeholder="User"
+          value={login}
+          onChange={(event) => setLogin(event.target.value)}
+        />
+        <Button to={`/${login}/repositories`}>
           <MdSearch size={42} />
         </Button>
       </Form>
